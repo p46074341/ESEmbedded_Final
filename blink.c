aa1b93e3baae46b63f44b56d7b4c03179e85c9ba
p46074341 <p46074341@mail.ncku.edu.tw>
2019-06-19 17:24:54 +0800

    finish. Thanks TA and lecturer in this semester

---
+++ blink.c
@@ -0,0 +1,105 @@
+#include <stdint.h>
+#include <stdio.h>
+#include "reg.h"
+#define UINT_MAX 1836311902
+
+/**
+ * 
+ * LED init
+ * 
+ */
+void led_init(unsigned int led)
+{
+	SET_BIT(RCC_BASE + RCC_AHB1ENR_OFFSET, GPIO_EN_BIT(GPIO_PORTD));
+
+	//MODER led pin = 01 => General purpose output mode
+	CLEAR_BIT(GPIO_BASE(GPIO_PORTD) + GPIOx_MODER_OFFSET, MODERy_1_BIT(led));
+	SET_BIT(GPIO_BASE(GPIO_PORTD) + GPIOx_MODER_OFFSET, MODERy_0_BIT(led));
+
+	//OT led pin = 0 => Output push-pull
+	CLEAR_BIT(GPIO_BASE(GPIO_PORTD) + GPIOx_OTYPER_OFFSET, OTy_BIT(led));
+
+	//OSPEEDR led pin = 00 => Low speed
+	CLEAR_BIT(GPIO_BASE(GPIO_PORTD) + GPIOx_OSPEEDR_OFFSET, OSPEEDRy_1_BIT(led));
+	CLEAR_BIT(GPIO_BASE(GPIO_PORTD) + GPIOx_OSPEEDR_OFFSET, OSPEEDRy_0_BIT(led));
+
+	//PUPDR led pin = 00 => No pull-up, pull-down
+	CLEAR_BIT(GPIO_BASE(GPIO_PORTD) + GPIOx_PUPDR_OFFSET, PUPDRy_1_BIT(led));
+	CLEAR_BIT(GPIO_BASE(GPIO_PORTD) + GPIOx_PUPDR_OFFSET, PUPDRy_0_BIT(led));
+}
+
+/**
+ * 
+ * blink LED forever
+ * 
+ */
+void blink(unsigned int led)
+{
+	led_init(led);
+
+	unsigned int i;
+
+	while (1)
+	{
+		//set GPIOD led pin
+		SET_BIT(GPIO_BASE(GPIO_PORTD) + GPIOx_BSRR_OFFSET, BSy_BIT(led));
+
+		for (i = 0; i < 500000; i++)
+			;
+
+		//reset GPIOD led pin
+		SET_BIT(GPIO_BASE(GPIO_PORTD) + GPIOx_BSRR_OFFSET, BRy_BIT(led));
+
+		for (i = 0; i < 500000; i++)
+			;
+	}
+}
+
+/**
+ * 
+ * blink LED x count
+ * 
+ */
+void blink_count(unsigned int led, unsigned int count)
+{
+	led_init(led);
+
+	unsigned int i;
+
+	while (count--)
+	{
+		//set GPIOD led pin
+		SET_BIT(GPIO_BASE(GPIO_PORTD) + GPIOx_BSRR_OFFSET, BSy_BIT(led));
+
+		for (i = 0; i < 500000; i++)
+			;
+
+		//reset GPIOD led pin
+		SET_BIT(GPIO_BASE(GPIO_PORTD) + GPIOx_BSRR_OFFSET, BRy_BIT(led));
+
+		for (i = 0; i < 500000; i++)
+			;
+	}
+}
+
+
+void fs(void)
+{
+	while (1)
+	{
+		int f1 = 0;
+		int f2 = 1;
+		int f3 = 1;
+		while (f3 > 0)
+		{	
+			int delay = 10000;
+			while(delay--)
+			;
+			printf("%d ",f3);
+			f3 = f1+f2;
+			f1 = f2;
+			f2 = f3;
+		}
+		printf("\r\n");
+	}
+}
